a7b6eadcfbe3c74a8a26575e974d7df9017c5a31
Robert Ware <robert.a.ware.95@gmail.com>
2024-04-14 15:31:34 -0700

    Swap X and Z axes in XYZConvert.cs calculations.
    This now allows you to:
    1. Generate some values into a given array using a nested XYZ loop.
    2. Index into that array using a Vector3I by calling XYZConvert.ToVector3I().
    3. Return the corresponding value that was generated by the XYZ nested loop at that particular iteration of X, Y, and Z.
    
    Previously a ZYX ordered nested loop was required to push values into an array and get the same results as using a flat loop with the of size X * Y * Z.

--- utils/XYZConvert.cs
+++ utils/XYZConvert.cs
@@ -9,19 +9,19 @@ namespace RawUtils
         // Convert XYZ coordinates in dimension space into a flat integer.
         public static int ToIndex(int X, int Y, int Z, Vector3I dimension)
         {
-            return X + (Y * dimension.X) + (Z * dimension.Y * dimension.X);
+            return Z + (Y * dimension.X) + (X * dimension.Y * dimension.Z);
         }
         public static int ToIndex(Vector3I XYZ, Vector3I dimension)
         {
-            return XYZ.X + (XYZ.Y * dimension.X) + (XYZ.Z * dimension.Y * dimension.X);
+            return ToIndex(XYZ.X, XYZ.Y, XYZ.Z, dimension);
         }
         
         // Convert an integer into XYZ coordinates in dimension space.
         public static Vector3I ToVector3I(int XYZ, Vector3I dimension)
         {
-            int X = XYZ % dimension.X;
-            int Y = XYZ / dimension.X % dimension.Y;
-            int Z = XYZ / dimension.X / dimension.Y % dimension.Z;
+            int X = XYZ / dimension.Z / dimension.Y % dimension.X;
+            int Y = XYZ / dimension.Z % dimension.Y;
+            int Z = XYZ % dimension.Z;
 
             return new Vector3I(X, Y, Z);
         }
